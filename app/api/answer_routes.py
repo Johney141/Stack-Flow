509d38b78a249719dc6d7e4eed873969d34dfe98
Jon M <jsm22601@gmail.com>
2024-09-05 19:24:15 -0400

    Added Create and update routes for answers

--- app/api/answer_routes.py
+++ app/api/answer_routes.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify
 from flask_login import current_user, login_required
-from app.models import Answer, User
+from app.models import Answer, User, db
+from app.forms import AnswerForm
 
 answer_routes = Blueprint('answers', __name__)
 
-
+# Get current users answers
 @answer_routes.route('/current')
 @login_required
 def user_answers():
@@ -26,7 +27,37 @@ def user_answers():
     }
     
     return jsonify(answer_res)
+
+
+# Update a answer
+@answer_routes.route('/<int:answer_id>', methods=['PUT'])
+@login_required
+def update_answer(answer_id):
+    answer = Answer.query.get(answer_id)
+    # If answer doesn't exist
+    if not answer:
+        return jsonify({"error": "Answer not found"})
     
+    # if not authorized
+    if answer.user_id != current_user.id:
+        return jsonify({"error": "Not Authorized to udpate answer"})
+
+    form = AnswerForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        answer.answer = form.data['answer']
+
+        db.session.commit()
+
+        res = {
+            "id": answer.id,
+            "userId": answer.id,
+            "questionId": answer.question_id,
+            "answer": answer.answer
+        }
+        return jsonify(res), 201
+    else: 
+        return form.errors, 401
 
     
 
