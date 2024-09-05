7a318932d598789976a69887851104d506861ff8
mylcd <yinlongmi@gmail.com>
2024-09-04 17:45:55 -0700

    added seeder for tags, question_tags

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,7 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .answers import seed_answers, undo_answers
 from .answer_comment import seed_answer_comments, undo_answer_comments
+from .tags import seed_tags, undo_tags
 
 from app.models.db import db, environment, SCHEMA
 
@@ -14,16 +15,18 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
         undo_answers()
         undo_answer_comments()
+        undo_tags()
     seed_users()
     seed_answers()
     seed_answer_comments()
+    seed_tags()
     # Add other seed functions here
 
 
@@ -33,4 +36,5 @@ def undo():
     undo_users()
     undo_answers()
     undo_answer_comments()
+    undo_tags()
     # Add other undo functions here
