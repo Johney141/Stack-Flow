1ec962ad96676368668e8d933f7b6ae38f3fa3bd
JamesAnderson30 <jamesanderson_18@hotmail.com>
2024-09-09 22:12:41 -0400

    once more

--- app/models/question.py
+++ app/models/question.py
@@ -11,7 +11,7 @@ class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(5000), nullable=False)
     subject = db.Column(db.String(250), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
 
 
     user = db.relationship('User', back_populates='questions')
