ca0c21595b81ad0d61005436d65ffd954b221c7c
JamesAnderson30 <jamesanderson_18@hotmail.com>
2024-09-11 19:14:03 -0400

    Implimented quesetion redux, added question list component and QuestionListItem child component.

--- react-vite/src/components/Questions/QuestionList.jsx
+++ react-vite/src/components/Questions/QuestionList.jsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 // import { useNavigate } from "react-router-dom";
-import { getAllTagsThunk } from "../../redux/tags";
-
+import { getAllQuestionsThunk } from "../../redux/questions";
+import QuestionListItem from './QuestionListItem';
 
 const QuestionList = () => {
     const [isLoaded, setIsLoaded] = useState(false);
-    const questions = useSelector(state => state.questionState.allTags)
+    const questions = useSelector(state => state.questionState.allQuestions)
     const dispatch = useDispatch();
 
     useEffect(() => {
         const getQuestions = async () => {
-            await dispatch(getAllTagsThunk())
+            await dispatch(getAllQuestionsThunk())
             setIsLoaded(true);
         }
 
@@ -26,10 +26,11 @@ const QuestionList = () => {
             <h1>Loading...</h1>
         )
     }
-    console.log(questions)
     return (
         <div className="question-list">
-            list
+            {questions.map((question_obj, idx)=>{
+                return (<QuestionListItem key={idx} question={{question_obj}}/>)
+            })}
         </div>
 
     )
