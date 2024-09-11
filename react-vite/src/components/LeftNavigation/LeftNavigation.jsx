db4cea330aa3b4adbcc0593cfa20a3ac83fae29f
mylcd <yinlongmi@gmail.com>
2024-09-11 01:52:29 -0700

    created left navbar

---
+++ react-vite/src/components/LeftNavigation/LeftNavigation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function LeftNavigation () {
+  return (
+    <nav class="leftnav">
+      <ul>
+        <li><a href='/'>Home</a></li>
+        <li><a href='/questions'>Questions</a></li>
+        <li><a href='/tags'>Tags</a></li>
+        <li><a href='/saved'>Saves</a></li>
+        <li><a href='/users'>Users</a></li>
+      </ul>
+    </nav>
+  );
+}
+export default LeftNavigation;
