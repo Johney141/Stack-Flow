70f076bef0e0ab72db81af935e3feaafb30b25a9
Yinlong Mi <145389601+mylcd@users.noreply.github.com>
2024-09-11 16:26:46 -0700

    left navbar, login modal (#38)
    * reworked login modal content and css
    
    * created left navbar
    
    * minor fixes, cleanup, and build

---
+++ react-vite/src/components/LeftNavigation/LeftNavigation.jsx
@@ -0,0 +1,14 @@
+function LeftNavigation () {
+  return (
+    <nav className="leftnav">
+      <ul>
+        <li><a href='/'>Home</a></li>
+        <li><a href='/questions'>Questions</a></li>
+        <li><a href='/tags'>Tags</a></li>
+        <li><a href='/saved'>Saves</a></li>
+        <li><a href='/users'>Users</a></li>
+      </ul>
+    </nav>
+  );
+}
+export default LeftNavigation;
